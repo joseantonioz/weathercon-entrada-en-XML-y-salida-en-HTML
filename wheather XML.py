fd016f89fb58b304a9cff52a53d8e6e846e1177e
José Antonio Zurita Gil <josan4zurita@hotmail.com>
2014-03-13 09:23:41 +0100

    Update wheather XML.py

--- wheather XML.py
+++ wheather XML.py
@@ -1,23 +1,51 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1 -strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"xml:lang="es">
-<meta charset="utf-8">
-    <head>
-          <title> El tiempo en Andalucia </title>
-    </head>
-    <body>
-        <h1>El tiempo en Andalucía</h1>
-        {% set count = 0 %}
-        {% for provincia in provincias %}
-          <h2>{{ ciudades }}</h2>
-        <ul>
-            
-            <li>Temperatura mínima prevista: {{ tempreminreal }} C</li>
-            <li>Temperatura máxima prevista: {{ tempremaxreal }} C</li>	
-            <li>Viento: {{ vientoreal }} </li>
-            
-        </ul>
-        {% set count = count + 1 %}
-        {% endfor %}
-    
-    </body>
-</html>
+#coding: utf-8
+import requests
+import webbrowser
+from lxml import etree
+from jinja2 import Template
+
+
+ciudades = ['Almeria','Cadiz','Cordoba','Huelva','Jaen','Malaga','Sevilla']
+plantilla = open("plantilla.xml","r")
+xml = ''
+
+tempemin = []
+tempemax = []
+viento = []
+direccionviento = []
+
+for ciudad in ciudades:
+
+
+valdef = {'q': '%s,spain' % ciudadp,'mode': 'xml','units': 'metric','lang': 'sp'}
+resp = requests.get('http://api.openweathermap.org/data/2.5/weather', params=p)
+
+raiz = etree.fromstring(r.text.encode("utf-8"))
+
+
+city = raiz.find("city")
+city.attrib["name"]
+temmin = raiz.find("temperature")
+temmin2 = int(float(temmin.attrib["min"]),1)
+temmax = raiz.find("temperature")
+temmax2 = int(float(temmax.attrib["max"]),1)
+wind = raiz.find("wind/speed")
+wind2 = round(float(wind.attrib["value"]),1)
+orient = raiz.find("wind/direction")
+orient2 = orientacion.attrib["name"]
+list_min.append(temmin2)
+list_max.append(temmax2)
+listspeed.append(wind2)
+listorientacion.append(orient2)
+
+
+
+for linea in plantilla
+        xml += plantilla
+        
+        
+plantillatem = Template(xtml)
+plantillatem = mitemplate.render(ciudades=ciudades,temp_min=list_min,temp_max=list_max,viento=listspeed,direccion=listorientacion)
+web.write(plantillatem)
+
+webbrowser.open('plantilla.xml')
